ddcc472a53914952f11e6f6aced5e2c081102b74
wlw81 <thomas.pasligh@gmail.com>
2020-04-01 16:13:23 +0200

    5.0 - major update: release of updated playerassignment view

--- teamme/src/main/java/de/pasligh/android/teamme/TeamSectionFragment.java
+++ teamme/src/main/java/de/pasligh/android/teamme/TeamSectionFragment.java
@@ -3,38 +3,52 @@ package de.pasligh.android.teamme;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import de.pasligh.android.teamme.backend.BackendFacade;
 import de.pasligh.android.teamme.objects.PlayerAssignment;
 import de.pasligh.android.teamme.tools.Flags;
 import de.pasligh.android.teamme.tools.TeamReactor;
+import de.pasligh.android.teamme.tools.TeamView_Interface;
 
 public class TeamSectionFragment extends Fragment implements View.OnClickListener {
 
     public static final String ARG_SECTION_NUMBER = "section_number";
 
+    public List<TeamView_Interface> getListener() {
+        if (listener == null) {
+            listener = new ArrayList<TeamView_Interface>();
+        }
+
+        return listener;
+    }
+
+    public void setListener(List<TeamView_Interface> listener) {
+        this.listener = listener;
+    }
+
+    private List<TeamView_Interface> listener;
+
     private List<PlayerAssignment> assignments;
     private LinearLayout layout;
     private BackendFacade facade;
-    private int sectionr;
+    private int sectionNr;
+    private AlertDialog alertToShow;
 
 
     public TeamSectionFragment() {
@@ -52,14 +66,14 @@ public class TeamSectionFragment extends Fragment implements View.OnClickListene
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        sectionr = (getArguments().getInt(ARG_SECTION_NUMBER));
-        Log.d(Flags.LOGTAG, "Create View " + sectionr);
+        sectionNr = (getArguments().getInt(ARG_SECTION_NUMBER));
+        Log.d(Flags.LOGTAG, "Create View " + sectionNr);
         View rootView = inflater.inflate(R.layout.fragment_team_overview,
                 container, false);
         layout = (LinearLayout) rootView
                 .findViewById(R.id.TeamOverviewSectionLayout);
         assignments = TeamReactor
-                .getAssignmentsByTeam(sectionr);
+                .getAssignmentsByTeam(sectionNr);
         for (PlayerAssignment assignmentToLayout : assignments) {
             RelativeLayout playerDetail = (RelativeLayout) inflater
                     .inflate(R.layout.fragment_player_detail, container,
@@ -87,62 +101,64 @@ public class TeamSectionFragment extends Fragment implements View.OnClickListene
         if (v instanceof ImageButton) {
             View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.dialog_assignment_change, (ViewGroup) v.getParent(), false);
             final PlayerAssignment pa = assignments.get(layout.indexOfChild(((View) v.getParent())));
+            final List<PlayerAssignment> assignmentsReOrdered = TeamReactor.getAssignmentsByTeam(sectionNr);
 
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setView(viewInflated);
             ToggleButton btn = viewInflated.findViewById(R.id.assignmentPromoteBT);
             TextView tv = viewInflated.findViewById(R.id.assignmentNameTV);
             tv.setText(pa.getPlayer().getName());
+            btn.setEnabled(assignmentsReOrdered.size() > 1);
             btn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    List<PlayerAssignment> assignmentsReOrdered = TeamReactor
-                            .getAssignmentsByTeam(sectionr);
-                    PlayerAssignment paReOrder = findAssignment(assignmentsReOrdered, pa.getId());
 
                     // rearrange
                     if (isChecked) {
-                        PlayerAssignment paOldCaptain = assignmentsReOrdered.remove(0);
-                        paOldCaptain.setOrderNumber(paReOrder.getOrderNumber());
-                        TeamReactor.getAssignmentsByTeam(sectionr).add(paOldCaptain);
+                        String oldCaptainName = assignmentsReOrdered.get(0).getPlayer().getName();
+                        String newCaptainName = pa.getPlayer().getName();
 
-                        paReOrder.setOrderNumber(1);
-                        Log.i(Flags.LOGTAG, "Team change: " + paReOrder.getPlayer() + " is now el capitano!");
+                        assignmentsReOrdered.get(0).getPlayer().setName(newCaptainName);
+                        assignmentsReOrdered.get(pa.getOrderNumber() - 1).getPlayer().setName(oldCaptainName);
+
+                        Log.i(Flags.LOGTAG, "Team change: " + pa.getPlayer() + " is now el capitano!");
                     } else {
-                        paReOrder.setOrderNumber(getFacade().getNextOrderNo(pa.getGame(), paReOrder.getTeam()));
-                        Log.i(Flags.LOGTAG, "Team change: " + paReOrder.getPlayer() + " is not the captain anymore");
+                        String oldCaptainName = pa.getPlayer().getName();
+                        String newCaptainName = assignmentsReOrdered.get(1).getPlayer().getName();
+
+                        assignmentsReOrdered.get(0).getPlayer().setName(newCaptainName);
+                        assignmentsReOrdered.get(1).getPlayer().setName(oldCaptainName);
+
+                        Log.i(Flags.LOGTAG, "Team change: " + pa.getPlayer() + " is not the captain anymore");
                     }
 
-                    // clean up to database
-                    int orderNo = 1;
-                    for (PlayerAssignment t : assignmentsReOrdered) {
-                        if (orderNo != t.getOrderNumber()) {
-                            getFacade().deleteAssignment(t.getOrderNumber(), t.getPlayer().getName());
-                            t.setOrderNumber(orderNo);
-                            t.setRevealed(true);
-                            getFacade().addPlayerAssignment(t);
+                    // and move this to the team reactor & backend
+                    if (getFacade().deleteAssignments(pa.getGame(), pa.getTeam())) {
+                        TeamReactor.overwriteAssignments(new HashSet<PlayerAssignment>(assignmentsReOrdered));
+                        for (PlayerAssignment playerAssignment : TeamReactor.getAssignmentsByTeam(pa.getTeam())) {
+                            getFacade().addPlayerAssignment(playerAssignment);
+                            Log.i(Flags.LOGTAG, "Replaced: " + pa.getPlayer());
                         }
-                        orderNo++;
                     }
-
-                    // and move this to the team reactor
-                    TeamReactor.overwriteAssignments(new HashSet<PlayerAssignment>(getFacade().getAssignments(pa.getGame())));
+                    if (alertToShow != null) {
+                        alertToShow.dismiss();
+                    }
+                    notifyListener();
                 }
             });
             btn.setChecked(pa.getOrderNumber() == 1);
-            //Toast.makeText(getContext(), .getPlayer().toString(), Toast.LENGTH_SHORT).show();
 
             Button btnKick = viewInflated.findViewById(R.id.assignmentKillBT);
+            btnKick.setEnabled(assignmentsReOrdered.size() > 1);
             btnKick.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     TeamReactor.getAssignments().remove(pa);
                     getFacade().deleteAssignment(pa.getGame(), pa.getPlayer().getName());
+                    alertToShow.dismiss();
                 }
             });
 
-            final AlertDialog alertToShow = builder.create();
-            alertToShow.getWindow().setSoftInputMode(
-                    WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+            alertToShow = builder.create();
             alertToShow.show();
 
         } else {
@@ -150,11 +166,9 @@ public class TeamSectionFragment extends Fragment implements View.OnClickListene
         }
     }
 
-    private PlayerAssignment findAssignment(List<PlayerAssignment> assignmentsReOrdered, int id) {
-        for (PlayerAssignment t : assignmentsReOrdered) {
-            if (t.getId() == id) {
-                return t;
-            }
+    public void notifyListener() {
+        for (TeamView_Interface t_listener : getListener()) {
+            t_listener.notifyAdapter();
         }
     }
 
